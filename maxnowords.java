c193e6b6a9f999585b722462254e7c6727b3c793
Diravia Sundaram <diraviasundaram@gmail.com>
2025-07-10 01:09:23 +0530

    solution for very easy leetcode problems

---
+++ maxnowords.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int mostWordsFound(String[] sentences) {
+        int max=Integer.MIN_VALUE;
+        for(int i=0;i<sentences.length;i++){
+            String s=sentences[i];
+            int count=0;
+            for(int j=0;j<s.length();j++){
+               if(s.charAt(j)==' '){
+                  count++;
+               }
+            }
+            count+=1;
+            if(count>max){
+                max=count;
+            }
+        }
+        return max;
+    }
+}
