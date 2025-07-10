84c460ebbc8fbb39c3ec76a119f67c4d44903359
Diravia Sundaram <diraviasundaram@gmail.com>
2025-07-10 21:28:15 +0530

    solution for very easy leetcode problems

---
+++ countkeychange.java
@@ -0,0 +1,14 @@
+class Solution {
+    public int countKeyChanges(String s) {
+       int count=0;
+       for(int i=1;i<s.length();i++){
+          if(Character.toLowerCase(s.charAt(i-1))==Character.toLowerCase(s.charAt(i))){
+            continue;
+          }
+          else{
+            count++;
+          }
+       } 
+       return count;  
+    }
+}
